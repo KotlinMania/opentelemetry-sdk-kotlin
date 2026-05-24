d0f9dd7f2bd807b1115c811f99f532fd7e74bf50
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-23 18:36:09 -0700

    Wire Kotlin → Swift Export → swift test loop into CI (#13)

--- build.gradle.kts
+++ build.gradle.kts
@@ -209,10 +209,18 @@ kotlin {
         binaries.framework { baseName = "OpentelemetrySdk"; xcf.add(this) }
     }
     iosSimulatorArm64 {
-        binaries.framework { baseName = "OpentelemetrySdk"; xcf.add(this) }
+        binaries.framework {
+            baseName = "OpentelemetrySdk"
+            isStatic = true
+            xcf.add(this)
+        }
     }
     iosX64 {
-        binaries.framework { baseName = "OpentelemetrySdk"; xcf.add(this) }
+        binaries.framework {
+            baseName = "OpentelemetrySdk"
+            isStatic = true
+            xcf.add(this)
+        }
     }
 
     tvosArm64 {
